623f8dd4d14064f7af1850de607570c04ad9c6f1
allen wes <allen.wes.g@gmail.com>
2015-07-13 11:29:48 -0500

    reverted changes to gulpserve

--- gulpfile.js
+++ gulpfile.js
@@ -36,25 +36,6 @@ gulp.task('webserver', function() {
 
 gulp.task('serve', ['js', 'webserver']);
 
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 function bundle() {
 	return b.bundle()
 	// log errors if they happen
