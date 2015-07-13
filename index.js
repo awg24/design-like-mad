2080eeef3fc4775c72b593f8e84c324813eef947
allen wes <allen.wes.g@gmail.com>
2015-07-13 11:35:11 -0500

    changed procfile

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('index.html')
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(process.env.PORT);
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 
