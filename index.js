16dfb1cb9af8fa4dd124d5c3ab10e8fb9f804e33
allen wes <allen.wes.g@gmail.com>
2015-07-13 11:52:41 -0500

    made changes to procfile

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
 
 app.get('/', function(request, response) {
-  response.render('index.html')
+  response.render('./index')
 });
-app.listen(process.env.PORT);
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+
+//app.listen(app.get('port'));
+console.log(app.get("port"))
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
